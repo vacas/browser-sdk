2c4cea1121a84539e8acbcf03350407686d6a7b0
Antoine Lévêque <antoine.leveque@datadoghq.com>
2020-07-17 15:19:13 +0200

    move components

--- extension/src/popup/src/App.tsx
+++ extension/src/popup/src/App.tsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect, useCallback } from 'react';
 import logo from './bits48.png';
 import './App.css';
 import Accordion from 'react-bootstrap/Accordion';
-import Card from 'react-bootstrap/Card';
-import Table from 'react-bootstrap/Table';
 
-import {ViewDetail } from './lib/rumEventsType'
+
+import { ViewDetail } from './lib/rumEventsType'
+import { ViewDetailCard } from './lib/viewDetailCard'
 
 const backgroundPageConnection = chrome.runtime.connect({ name: 'name' });
 
@@ -13,9 +13,9 @@ export default function App() {
   const [viewDetails, setViewDetail] = useState<ViewDetail[]>([]);
 
   const listener = useCallback((request) => {
-    switch(request.type) {
+    switch (request.type) {
       case 'viewDetails':
-      setViewDetail(request.payload as ViewDetail[])
+        setViewDetail(request.payload as ViewDetail[])
         break;
       default:
         break;
@@ -42,71 +42,16 @@ export default function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        {ViewDetailsComponent(viewDetails)}
+        {viewDetails &&
+          <Accordion className="App-view-accordion">
+            {viewDetails.map((viewDetail: ViewDetail) => {
+              return (<ViewDetailCard key={viewDetail.id} viewDetail={viewDetail} />);
+
+            })}
+          </Accordion>
+        }
         <p>Refreshing every 1s</p>
       </header>
     </div>
   );
-}
-
-function ViewDetailsComponent(viewDetails: ViewDetail[]){
-  if(viewDetails){
-    return (viewDetails && (
-      <Accordion className="App-view-accordion">
-        {viewDetails.map((viewDetail: ViewDetail) => {
-        return (<ViewDetailCard key={viewDetail.id} viewDetail={viewDetail}/>);
-
-      })}
-      </Accordion>
-      )
-    )
-  }
-
-  return <></>
-}
-
-interface ViewDetailCardProps{
-  viewDetail: ViewDetail;
-}
-
-function ViewDetailCard({viewDetail}: ViewDetailCardProps){
-  return (
-    <Card className="App-view-card">
-      <Accordion.Toggle as={Card.Header} eventKey={viewDetail.id} className="App-view-card-header">
-         {viewDetail.description} - {formatDate(viewDetail.date)}
-        </Accordion.Toggle>
-      <Accordion.Collapse eventKey={viewDetail.id}>
-        <Card.Body className="App-view-card-body">
-          <ViewDetailExpanded viewDetail={viewDetail}/>
-        </Card.Body>
-      </Accordion.Collapse>
-  </Card>
-  )
-}
-
-function formatDate(date: number) {
-  return new Date(date).toLocaleTimeString()
-}
-
-function ViewDetailExpanded({viewDetail}: ViewDetailCardProps){
-  return (
-  <Table striped bordered hover variant="dark">
-    <thead>
-      <tr>
-        <th>description</th>
-        <th>date</th>
-      </tr>
-    </thead>
-    <tbody>
-    {viewDetail && viewDetail.events && viewDetail.events.map((event: any) => {
-      return (
-        <tr>
-        <td>{event.description.substring(0, 100)}</td>
-        <td>{formatDate(event.date)}</td>
-      </tr>
-    )
-    })}
-    </tbody>
-  </Table>
-  )
 }
---
+++ extension/src/popup/src/lib/viewDetailCard.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Accordion from 'react-bootstrap/Accordion';
+import Table from 'react-bootstrap/Table';
+
+import { ViewDetail } from './rumEventsType'
+
+interface ViewDetailCardProps {
+    viewDetail: ViewDetail;
+}
+
+
+function formatDate(date: number) {
+    return new Date(date).toLocaleTimeString()
+}
+
+
+
+export const ViewDetailCard = ({ viewDetail }: ViewDetailCardProps)  => {
+    return (
+        <Card className="App-view-card">
+            <Accordion.Toggle as={Card.Header} eventKey={viewDetail.id} className="App-view-card-header">
+                {viewDetail.description} - {formatDate(viewDetail.date)}
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey={viewDetail.id}>
+                <Card.Body className="App-view-card-body">
+                    <ViewDetailExpanded viewDetail={viewDetail} />
+                </Card.Body>
+            </Accordion.Collapse>
+        </Card>
+    )
+}
+
+const ViewDetailExpanded = ({ viewDetail }: ViewDetailCardProps) => {
+    return (
+        <Table striped bordered hover variant="dark">
+            <thead>
+                <tr>
+                    <th>description</th>
+                    <th>date</th>
+                </tr>
+            </thead>
+            <tbody>
+                {viewDetail && viewDetail.events && viewDetail.events.map((event: any) => {
+                    return (
+                        <tr>
+                            <td>{event.description.substring(0, 100)}</td>
+                            <td>{formatDate(event.date)}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </Table>
+    )
+}
