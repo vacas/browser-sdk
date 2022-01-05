ffe8d4ed33190088f2c0fc9249a4a44495752321
Bastien Caudan <bastien.caudan@gmail.com>
2022-01-05 18:29:11 +0100

    👌 fix new lint rules

--- test/browsers.conf.js
+++ test/browsers.conf.js
@@ -1,49 +1,10 @@
 // Capabilities: https://www.browserstack.com/automate/capabilities
 
 module.exports = {
-  EDGE: {
-    base: 'BrowserStack',
-    browser: 'Edge',
-    browser_version: '86',
-    os: 'Windows',
-    os_version: '10',
-  },
   FIREFOX: {
     base: 'BrowserStack',
     browser: 'Firefox',
-    browser_version: '67.0',
-    os: 'Windows',
-    os_version: '10',
-  },
-  SAFARI: {
-    base: 'BrowserStack',
-    browser: 'Safari',
-    browser_version: '12.0',
-    os: 'OS X',
-    os_version: 'Mojave',
-  },
-  CHROME_MOBILE: {
-    base: 'BrowserStack',
-    os: 'android',
-    os_version: '10.0',
-    browser: 'android',
-    device: 'Google Pixel 4',
-    browser_version: null,
-    real_mobile: true,
-  },
-  SAFARI_MOBILE: {
-    base: 'BrowserStack',
-    os: 'ios',
-    os_version: '12',
-    device: 'iPhone XR',
-    browser: 'iPhone',
-    browser_version: null,
-    real_mobile: true,
-  },
-  IE_11: {
-    base: 'BrowserStack',
-    browser: 'IE',
-    browser_version: '11.0',
+    browser_version: '78.0',
     os: 'Windows',
     os_version: '10',
   },
